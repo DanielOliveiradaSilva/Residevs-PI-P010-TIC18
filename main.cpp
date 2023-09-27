e0b59c7c363521ee59186cea0d263885c587f6b0
daniel <danieloliveira.s558@gmail.com>
2023-09-26 22:25:05 -0300

    atualizado

--- main.cpp
+++ main.cpp
@@ -13,6 +13,7 @@ struct Aluno{
 };
 
 void listarDados(const vector<Aluno> &alunos);//lista nome, nota1, nota2. Mais pode ser implementado para listar media.
+void inserirAluno(vector<Aluno> &alunos);
 void calcularMedia(vector<Aluno>&alunos); //A ideia era usar para inseri a medias de todos os alunos.Mais ṕode ser implementado ou criado outra função para media individual por exemplo. 
 int buscarAluno(vector<Aluno>&alunos, string nome);//retona o indice do aluno
 void alterarAluno(vector<Aluno>&alunos);//Precisa atualizar a media quando atualizar a nota? Devo criar uma função para isso?
@@ -57,4 +58,18 @@ int main(){
     //--------------------Fim---------------------
 
     return 1;
+}
+
+void inserirAluno(vector<Aluno> &alunos){
+       
+    Aluno novo_aluno;
+    cout << "Digite o nome do Aluno:\n";
+    cin.ignore(); // Limpar o buffer de entrada antes de ler a linha completa
+    getline(cin, novo_aluno.nome);//para ler o nome completo do Aluno, permitindo espaços. 
+    cout << "Digite a nota 1: ";
+    cin >> novo_aluno.nota1;
+    cout << "Digite a nota 2: ";
+    cin >> novo_aluno.nota2;
+    alunos.push_back(novo_aluno);
+      
 }
