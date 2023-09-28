af6b6b758698eb534251d1f7f2fdc721e065a52b
leogmc <ifbaianoleo@gmail.com>
2023-09-28 02:19:56 -0300

    Iniciando etapa individual

--- main.cpp
+++ main.cpp
@@ -4,72 +4,39 @@
 
 using namespace std;
 
-// Estrutura https://drive.google.com/file/d/1DOSEwaityLckkWh3SqNXxnqVoSlRgQBL/view?usp=sharing
 struct Aluno{
-   string nome;
-   double nota1;
-   double nota2;
-   double media;
+    string nome;
+    float nota1;
+    float nota2;
 };
 
-void listarDados(const vector<Aluno> &alunos);//lista nome, nota1, nota2. Mais pode ser implementado para listar media.
-void inserirAluno(vector<Aluno> &alunos);
-void calcularMedia(vector<Aluno>&alunos); //A ideia era usar para inseri a medias de todos os alunos.Mais ṕode ser implementado ou criado outra função para media individual por exemplo. 
-int buscarAluno(vector<Aluno>&alunos, string nome);//retona o indice do aluno
-void alterarAluno(vector<Aluno>&alunos);//Precisa atualizar a media quando atualizar a nota? Devo criar uma função para isso?
-void exluirAluno(vector<Aluno> &alunos);//Precisa reordenar? Mesmo que eu exluia uma valor de uma lista ordenada ela continua ordenada. A esta está ordenada antes de excluir? 
-void bubblesort(vector<Aluno> &alunos);//ordenar em ordem lexografica.
+vector<Aluno> alunos;
+// cin.ignore(); // Limpar o buffer de entrada antes de ler a linha completa
+// getline(cin, novo_aluno.nome);//para ler o nome completo do Aluno, permitindo espaços.
+
+
 
 int main(){
-    //Fluxograma do algortimo  https://drive.google.com/file/d/1hmIH267o7IIGC-jx0_fUVAD5jitPDvvV/view
+    
+    int limite_alunos;
+    Aluno aluno;
 
-    vector<Aluno> alunos;
-    Aluno novo_aluno;
-    //---------------Primeiro Aluno----------------
-    novo_aluno.nome = "Daniel Oliveira da Silva";
-    novo_aluno.nota1 = 8.3;
-    novo_aluno.nota2 = 7.4;
-    novo_aluno.media = (novo_aluno.nota1 + novo_aluno.nota2)/2;
-    alunos.push_back(novo_aluno);//inserção.
-    //--------------------Fim---------------------
+        cout << "Insira o limite de alunos: ";
+        cin >> limite_alunos;
 
-    //---------------Segunto Aluno----------------
-    novo_aluno.nome = "Leonardo Garcia";
-    novo_aluno.nota1 = 9.3;
-    novo_aluno.nota2 = 8.4;
-    novo_aluno.media = (novo_aluno.nota1 + novo_aluno.nota2)/2;
-    alunos.push_back(novo_aluno);//inserção.
-    //--------------------Fim---------------------
 
-    //---------------Terceiro Aluno---------------
-    novo_aluno.nome = "Lorena Andrade";
-    novo_aluno.nota1 = 9.8;
-    novo_aluno.nota2 = 6.4;
-    novo_aluno.media = (novo_aluno.nota1 + novo_aluno.nota2)/2;
-    alunos.push_back(novo_aluno);//inserção.
-    //--------------------Fim--------------------
+    for(int i = 0; i < limite_alunos; i++){
 
-    //---------------Quarto Aluno----------------
-    novo_aluno.nome = "Lorena Andrade";
-    novo_aluno.nota1 = 9.8;
-    novo_aluno.nota2 = 6.4;
-    novo_aluno.media = (novo_aluno.nota1 + novo_aluno.nota2)/2;
-    alunos.push_back(novo_aluno);//inserção.
-    //--------------------Fim---------------------
+        cout << "Insira o nome do aluno " << i+1 << ": ";
+        cin.ignore();
+        getline(cin, aluno.nome);
 
-    return 1;
-}
+        cout << "Insira a nota [1] do aluno: ";
+        cin >> aluno.nota1;
 
-void inserirAluno(vector<Aluno> &alunos){
-       
-    Aluno novo_aluno;
-    cout << "Digite o nome do Aluno:\n";
-    cin.ignore(); // Limpar o buffer de entrada antes de ler a linha completa
-    getline(cin, novo_aluno.nome);//para ler o nome completo do Aluno, permitindo espaços. 
-    cout << "Digite a nota 1: ";
-    cin >> novo_aluno.nota1;
-    cout << "Digite a nota 2: ";
-    cin >> novo_aluno.nota2;
-    alunos.push_back(novo_aluno);
-      
+        cout << "Insira a nota [2] do aluno: ";
+        cin >> aluno.nota2;
+    }
+
+ return 0;
 }
