c03064173a20122c87723a66da28572189ddbe99
leogmc <ifbaianoleo@gmail.com>
2023-09-28 04:12:22 -0300

    criando funcoes: localizar aluno e excluir aluno

--- main.cpp
+++ main.cpp
@@ -17,7 +17,7 @@ void listarAlunos(vector<Aluno> alunos);//lista nome, nota1, nota2. Mais pode se
 void calcularMedia(vector<Aluno>&alunos); //A ideia era usar para inseri a medias de todos os alunos.Mais ṕode ser implementado ou criado outra função para media individual por exemplo. 
 int buscarAluno(vector<Aluno>&alunos, string nome);//retona o indice do aluno
 void alterarAluno(vector<Aluno>&alunos);//Precisa atualizar a media quando atualizar a nota? Devo criar uma função para isso?
-void exluirAluno(vector<Aluno> &alunos);//Precisa reordenar? Mesmo que eu exluia uma valor de uma lista ordenada ela continua ordenada. A esta está ordenada antes de excluir? 
+void exluirAluno(vector<Aluno> &alunos, string nome);//Precisa reordenar? Mesmo que eu exluia uma valor de uma lista ordenada ela continua ordenada. A esta está ordenada antes de excluir? 
 void bubblesort(vector<Aluno> &alunos);//ordenar em ordem lexografica.
 void incluirParaTeste(vector<Aluno> &alunos);//Função que insere alguns alunos e suas notas
 int main(){
@@ -163,3 +163,35 @@ void incluirParaTeste(vector<Aluno> &alunos){
     //--------------------Fim---------------------
 
 }
+
+int buscarAluno(vector<Aluno>&alunos, string nome){
+
+    string nome_aluno;
+    
+    cout << "Insira o nome do aluno que deseja localizar: ";
+    getline(cin, nome_aluno);
+
+    for(int i = 0; i < alunos.size() ; i++ ){
+        if(nome_aluno == alunos[i].nome){
+            cout << "Aluno encontrado!" << endl;
+            return i;
+        } 
+
+    }
+cout << "Aluno não encontrado." << endl;
+return -1;
+}
+
+void exluirAluno(vector<Aluno> &alunos, string nome){
+
+    int indice = buscarAluno(alunos, nome);
+
+    if (indice != -1){
+        alunos.erase(alunos.begin() + indice);
+        cout << "Aluno removido com sucesso!" << endl;
+    } else {
+        cout << "Aluno não encontrado, portanto nenhum aluno foi removido." << endl;
+    }
+
+}
+
