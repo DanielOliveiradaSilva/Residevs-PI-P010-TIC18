8b19f858aadbb16978a01d52f53b7270a23fd487
andre10dias <andre10dias@ymail.com>
2023-10-04 20:04:07 -0300

    Etapas 1 e 4 individual

--- main_carlos.cpp
+++ main_carlos.cpp
@@ -11,16 +11,66 @@ struct Aluno{
    double media;
 };
 
+void cabecalhoAluno();
 bool inserirAluno(vector<Aluno> &alunos, int &limite, Aluno aluno);
 void interfaceAluno(vector<Aluno> &alunos);
+void cabecalhoListar();
+int listarAlunos(vector<Aluno> &alunos);
+void menu();
+int sair();
+void systemClear();
+void systemPause();
+void systemPauseAndClear();
 
-int main(){
-    vector<Aluno> alunos;
+#define MEDIA_APROVACAO 7
 
-    interfaceAluno(alunos);
-
+int main(void) {
+    menu();
     cout << endl;
     return 0;
+}
+
+void menu() {
+    vector<Aluno> alunos;
+    int opcao;
+
+    do
+    {
+        systemClear();
+        cout << "------------------------------------------------------------" << endl;
+        cout << "\tSistema de controle de notas de estudantes" << endl;
+        cout << "------------------------------------------------------------" << endl << endl;
+        
+        cout << "[ 1 ] Inserir aluno\n[ 2 ] Listar alunos\n[ 0 ] Sair\n" << endl;
+        cout << "Selecione uma opção: ";
+        cin >> opcao;
+
+        switch (opcao)
+        {
+            case 1:
+                interfaceAluno(alunos);
+                break;
+
+            case 2:
+                listarAlunos(alunos);
+                break;
+
+            case 0:
+                sair();
+                break;
+            
+            default:
+                cout << "\nOpção inválida!\n";
+                systemPause();
+                break;
+        }
+    } while (opcao != 0);
+}
+
+void cabecalhoAluno() {
+    cout << "------------------------------------------------------------" << endl;
+    cout << "\t\t\tInserir alunos" << endl;
+    cout << "------------------------------------------------------------" << endl << endl;
 }
 
 void interfaceAluno(vector<Aluno> &alunos) {
@@ -32,10 +82,15 @@ void interfaceAluno(vector<Aluno> &alunos) {
 
     do
     {
+        systemClear();
+        cabecalhoAluno();
+
         if (tamanhoAtual < limite)
         {
-            cout << "\n\nDeseja inserir um aluno (s/n)? ";
+            cout << "\nDeseja inserir um aluno (s/n)? ";
             cin >> opcao;
+            systemClear();
+            cabecalhoAluno();
         }
         
         if (tamanhoAtual < limite && (opcao == 's' || opcao == 'S'))
@@ -44,17 +99,25 @@ void interfaceAluno(vector<Aluno> &alunos) {
             {
                 cout << "\n\nInforme um limite de alunos: ";
                 cin >> limite;
+                systemClear();
+                cabecalhoAluno();
             }
 
             cout << "\n\nDigite o nome do Aluno: ";
             cin.ignore();
             getline(cin, aluno.nome);
+            systemClear();
+            cabecalhoAluno();
 
             cout << "\nDigite a nota 1: ";
             cin >> aluno.nota1;
+            systemClear();
+            cabecalhoAluno();
 
             cout << "\nDigite a nota 2: ";
             cin >> aluno.nota2;
+            systemClear();
+            cabecalhoAluno();
             
             alunoInserido = inserirAluno(alunos, limite, aluno); 
             tamanhoAtual = alunos.size();
@@ -62,10 +125,12 @@ void interfaceAluno(vector<Aluno> &alunos) {
             if (alunoInserido)
             {
                 cout << "\n\nAluno adicionado com sucesso.\n";
+                systemPauseAndClear();
             }
             else 
             {
                 cout << "\n\nFalha ao inserir aluno.\n";
+                systemPauseAndClear();
             }
         }
         
@@ -73,7 +138,9 @@ void interfaceAluno(vector<Aluno> &alunos) {
 
     if (tamanhoAtual >= limite)
     {
+        cabecalhoAluno();
         cout << "\n\nLimite de alunos alcançado.\n";
+        systemPauseAndClear();
     }
 }
 
@@ -89,4 +156,64 @@ bool inserirAluno(vector<Aluno> &alunos, int &limite, Aluno aluno) {
     }
 
     return alunoInserido;
+}
+
+void cabecalhoListar() {
+    cout << "------------------------------------------------------------" << endl;
+    cout << "\t\t\tListar alunos" << endl;
+    cout << "------------------------------------------------------------" << endl << endl;
+}
+
+int listarAlunos(vector<Aluno> &alunos) {
+    int tamanho = alunos.size();
+    string situacao;
+
+    systemClear();
+    cabecalhoListar();
+
+    if (tamanho > 0)
+    {
+        cout << "Nome\t\tNota 1\tNota 2\tMédia\tsituação" << endl;
+        for (Aluno aluno : alunos)
+        {
+            situacao = aluno.media >= MEDIA_APROVACAO ? "APROVADO" : "REPROVADO";
+            cout << aluno.nome << "\t\t" << aluno.nota1 << "\t" << aluno.nota2 << "\t" << aluno.media << "\t" << situacao << endl;
+        }
+    }
+    else
+    {
+        cout << "\nNenhum dado encontrado\n";
+    }
+
+    systemPauseAndClear();
+    return 0;
+}
+
+int sair() {
+    cout << "\nFinalizando programa...";
+    systemPauseAndClear();
+    return 0;
+}
+
+void systemClear() {
+    #if __linux__
+        system("clear");
+    #else
+        system("cls");
+    #endif
+}
+
+void systemPause() {
+    cout << "\nPressione ENTER para continuar...\n";
+    #if __linux__
+        cin.ignore();
+    #else
+        cin.sync();
+    #endif
+    cin.get();
+}
+
+void systemPauseAndClear() {
+    systemPause();
+    systemClear();
 }
